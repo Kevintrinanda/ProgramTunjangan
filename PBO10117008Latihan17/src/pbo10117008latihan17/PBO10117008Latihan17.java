e435b3b1eca99e238aaf34b488390c484640663d
Kevin Trinanda <56400686+Kevintrinanda@users.noreply.github.com>
2019-10-10 21:47:16 +0700

    Add files via upload

---
+++ PBO10117008Latihan17/src/pbo10117008latihan17/PBO10117008Latihan17.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10117008latihan17;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+public class PBO10117008Latihan17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public String nama;
+    public int id;
+    public int gajipokok;
+    public int tunjangan;
+    public int totalgaji;
+    
+    public void isi_data{}
+    {
+          Scanner keyboard = new Scanner(System.in);
+        System.out.println("=======Program Tunjangan=======");
+        System.out.println("Berapa gaji anda perbulan? ");
+        id = keyboard.nextInt();
+        System.out.println("Status Anda? (Menikah/Belum)");
+        nama = keyboard.next();
+        
+        {
+            public int gajipokok{}
+            
+        }
+            int gaji = id;
+            return tunjangan;
+}
+}
+    
+    public static void main(String[] args) {
+ 
+        
+    }
+    
+}
